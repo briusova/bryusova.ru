62b6cde35e7eef1a2ce9fda1684979b25c7f32ea
briusova <briusova@gmail.com>
2025-01-18 18:57:25 +0200

    статьи с фото

--- pages/62d5be3e-6e81-4990-af5c-bf1d8b27fa30.vue
+++ pages/62d5be3e-6e81-4990-af5c-bf1d8b27fa30.vue
@@ -4,20 +4,32 @@
             <div class="max-w-xl space-y-3 not-prose mb-6">
                 <h3 class="text-indigo-600 font-semibold">{{ the.name }}</h3>
                 <p class="text-gray-800 text-3xl font-semibold sm:text-4xl">{{ the.title }}</p>
-                <p>{{ the.description }} Больше статей на сайте психологов №1 <a
-                        href="https://www.b17.ru/briusova" target="_blank" rel="noopener noreferrer" text-cyan-600=""
+                <p>{{ the.description }} Больше статей на сайте психологов №1 <a href="https://www.b17.ru/briusova"
+                        target="_blank" rel="noopener noreferrer" text-cyan-600=""
                         hover:text-blue-600="">https://www.b17.ru/briusova</a>, а также в моём <a
                         href="https://t.me/abb_psy" target="_blank" rel="noopener noreferrer" text-cyan-600=""
                         hover:text-blue-600="">Телеграм-канале</a>, где я делюсь актуальными материалами и советами.</p>
             </div>
 
 
-            <ul class="not-prose list-disc list-inside">
-                <li v-for="{ header, to } in the.$children"><el-button tag="router-link" link
-                        :to="to">{{ header }}</el-button></li>
-            </ul>
 
-            
+
+
+
+
+            <ul class="divide-y divide-slate-100 not-prose">
+                <li v-for="{ header, to, description, images } in the.$children">
+                    <RouterLink :to="to" class="flex items-start gap-4 px-4 py-3">
+                        <div class="flex items-center shrink-0">
+                            <img :src="images[0]?.url" :alt="images[0]?.alt" class="w-20 rounded">
+                        </div>
+                        <div class="flex flex-col gap-0 min-h-[2rem] items-start justify-center w-full min-w-0">
+                            <h4 class="text-base text-slate-700 ">{{ header }}</h4>
+                            <p class="w-full text-sm text-slate-500">{{ description }}</p>
+                        </div>
+                    </RouterLink>
+                </li>
+            </ul>
 
 
         </div>
