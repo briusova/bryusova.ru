bd463f7d0ab1ee4ea5e5f06ea039e1ba369cd7e4
briusova <briusova@gmail.com>
2025-01-23 12:06:44 +0200

    анонс

--- pages/5456fed9-e078-4d90-82fe-2fb2c49feae7.vue
+++ pages/5456fed9-e078-4d90-82fe-2fb2c49feae7.vue
@@ -1,10 +1,11 @@
 <template>
+
+
     <section class="not-prose py-14">
         <div class="max-w-screen-xl mx-auto md:px-8">
             <div class="items-center gap-x-12 sm:px-4 md:px-0 lg:flex">
                 <div class="flex-1 sm:hidden lg:block">
-                    <img :src="the.images[0].url"
-                        class="md:max-w-lg sm:rounded-lg" alt="">
+                    <img :src="the.images[0].url" class="md:max-w-lg sm:rounded-lg" alt="">
                 </div>
                 <div class="max-w-xl px-4 space-y-3 mt-6 sm:px-0 md:mt-0 lg:max-w-2xl">
                     <h3 class="text-indigo-600 font-semibold">
@@ -14,12 +15,17 @@
                         {{ the.title }}
                     </p>
                     <p class="mt-3 text-gray-600">
-                        {{ the.description }}
-                    </p>
-                    <router-link :to="the.$children[0].to"
-                        class="inline-flex gap-x-1 items-center text-indigo-600 hover:text-indigo-500 duration-150 font-medium">
+                    </p><p>Владение навыками эмоциональной саморегуляции — ключ к гармонии в жизни и инструмент для
+                        достижения своих целей. Обучение проверенным практическим методам позволяет управлять своими
+                        эмоциями, справляться с тревожными состояниями и сложными жизненными ситуациями. Марафон «Мой
+                        внутренний ресурс» — отличная возможность:&nbsp;</p><p>🔸<span style="color: var(--un-prose-body);">&nbsp;узнать, как формируется тревожность <br>🔸 научиться
+                        принимать себя, свои чувства и эмоции <br>🔸 нарастить ресурс, вернуть себе уверенность</span></p>
+                    <p></p>
+                    <router-link :to="the.$children[0].to" class="inline-flex gap-x-1 items-center text-indigo-600 hover:text-indigo-500 duration-150 font-medium">
                         подробнее
-                        <el-icon><Right /></el-icon>
+                        <el-icon>
+                            <right>
+                        </right></el-icon>
                     </router-link>
                 </div>
             </div>
