680deb290516668ce9ee332c654b6632c214c8e4
Jerry Bruwes <jbruwes@icloud.com>
2025-02-21 17:20:55 +0200

    form

--- pages/e18944a7-e929-4666-a141-25349dc34f50.vue
+++ pages/e18944a7-e929-4666-a141-25349dc34f50.vue
@@ -1,71 +1,153 @@
 <template>
-  <main class="py-14 not-prose" un-cloak>
-    <div class="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
-      <div class="rounded-3xl overflow-hidden shadow-2xl mb-24">
-        <img src="my_images/veAOAZWU-Ys.jpg" alt="" decoding="async" class="h-auto min-w-2xl">
-      </div>
-      <div class="max-w-xl space-y-3">
-        <h3 class="text-indigo-600 font-semibold">{{ the.name }}</h3>
-        <p class="text-gray-800 text-3xl font-semibold sm:text-4xl">{{ the.title }}</p>
-        <p>{{ the.description }}</p>
-      </div>
-      <div>
-        <ul class="mt-12 flex flex-wrap gap-x-12 gap-y-6 items-center lg:gap-x-24">
-          <li v-for="(item, idx) in contactMethods" :key="idx">
-            <h4 class="text-gray-800 text-lg font-medium">{{ item.title }}</h4>
-            <div class="mt-3 flex items-center gap-x-3">
-              <icon class="text-gray-400 size-7" :icon="item.icon"></icon>
-              <a :target="item.target && '_blank'" rel="noopener noreferrer" :href="item.href">{{ item.contact }}</a>
-            </div>
-          </li>
-        </ul>
-      </div>
+  <div class="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8" un-cloak>
+    <div class="rounded-3xl overflow-hidden shadow-2xl mb-24">
+      <img src="my_images/veAOAZWU-Ys.jpg" alt="" decoding="async" class="h-auto min-w-2xl">
     </div>
-  </main>
+    <div class="max-w-xl space-y-3">
+      <h3 class="text-indigo-600 font-semibold">{{ name }}</h3>
+      <p class="text-gray-800 text-3xl font-semibold sm:text-4xl">{{ title }}</p>
+      <p>{{ description }}</p>
+    </div>
+    <div class="mt-24 grid grid-cols-4 gap-12 md:grid-cols-8 lg:grid-cols-12">
+      <el-form :model="form" label-width="auto" ref="formRef" class="col-span-4 lg:col-span-6" label-position="top">
+        <el-form-item label="Ваше имя" prop="name" :rules="[
+          {
+            required: true,
+            message: 'Пожалуйста, введите своё имя',
+            trigger: 'blur',
+          },
+        ]">
+          <el-input v-model="form.name" />
+        </el-form-item>
+        <el-form-item label="Ваш телефон" prop="phone">
+          <el-input v-model="form.phone" />
+        </el-form-item>
+        <el-form-item label="Ваш емейл" prop="email" :rules="[
+          {
+            required: true,
+            message: 'Пожалуйста, введите адрес электронной почты',
+            trigger: 'blur',
+          },
+          {
+            type: 'email',
+            message: 'Пожалуйста, введите корректный адрес электронной почты',
+            trigger: ['blur', 'change'],
+          },
+        ]">
+          <el-input v-model="form.email" />
+        </el-form-item>
+        <el-form-item label="Сообщение" prop="desc" :rules="[
+          {
+            required: true,
+            message: 'Пожалуйста, введите своё сообщение',
+            trigger: 'blur',
+          },
+        ]">
+          <el-input v-model="form.desc" type="textarea" />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm(formRef)">Отправить заявку</el-button>
+          <el-button @click="resetForm(formRef)">Очистить форму</el-button>
+        </el-form-item>
+      </el-form>
+      <ul class="flex flex-wrap gap-x-12 gap-y-6 items-center lg:gap-x-24 col-span-4 lg:col-span-6">
+        <li v-for="(item, idx) in contactMethods" :key="idx">
+          <h4 class="text-gray-800 text-lg font-medium">{{ item.title }}</h4>
+          <div class="mt-3 flex items-center gap-x-3">
+            <icon class="text-gray-400 size-7" :icon="item.icon"></icon>
+            <a :target="item.target && '_blank'" rel="noopener noreferrer" :href="item.href">{{ item.contact }}</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { inject } from "vue";
-const { id } = defineProps(["id"]);
-const pages = inject("pages");
-const the = pages[id];
-const contactMethods = [
-  {
-    icon: "la:map-marker",
-    contact: "ул. Степана Разина, д. 20",
-    title: "Калининград"
-  },
-  {
-    icon: "la:map-marker",
-    contact: "площадь Журавлёва, д. 10",
-    title: "Москва"
-  },
-  {
-    icon: "la:phone",
-    contact: "+7 (906) 792-1244",
-    title: "Телефон",
-    href: "tel:+79067921244"
-  },
-  {
-    icon: "la:envelope",
-    contact: "briusova@gmail.com",
-    title: "E-mail",
-    href: "mailto:briusova@gmail.com"
-  },
-  {
-    icon: "la:telegram",
-    contact: "https://t.me/abb_psy",
-    title: "Телеграм",
-    href: "https://t.me/abb_psy",
-    target: true
-  },
-  {
-    icon: "la:vk",
-    contact: "https://vk.com/abb_psy",
-    title: "ВКонтакте",
-    href: "https://vk.com/abb_psy",
-    target: true
-  },
-];
+import { reactive, ref, inject } from "vue";
+import { ElMessage } from "element-plus";
 
+
+const { id } = defineProps(["id"]),
+  { title: pageTitle, name, title, description } = inject("pages")[id],
+  formRef = ref(),
+  method = "POST",
+  headers = {
+    'Content-Type': 'application/json'
+  },
+  form = reactive({
+    name: "",
+    phone: "",
+    email: "",
+    desc: '',
+  }),
+  message = "Заявка отправлена",
+  contactMethods = [
+    {
+      icon: "la:map-marker",
+      contact: "ул. Степана Разина, д. 20",
+      title: "Калининград"
+    },
+    {
+      icon: "la:map-marker",
+      contact: "площадь Журавлёва, д. 10",
+      title: "Москва"
+    },
+    {
+      icon: "la:phone",
+      contact: "+7 (906) 792-1244",
+      title: "Телефон",
+      href: "tel:+79067921244"
+    },
+    {
+      icon: "la:envelope",
+      contact: "briusova@gmail.com",
+      title: "E-mail",
+      href: "mailto:briusova@gmail.com"
+    },
+    {
+      icon: "la:telegram",
+      contact: "https://t.me/abb_psy",
+      title: "Телеграм",
+      href: "https://t.me/abb_psy",
+      target: true
+    },
+    {
+      icon: "la:vk",
+      contact: "https://vk.com/abb_psy",
+      title: "ВКонтакте",
+      href: "https://vk.com/abb_psy",
+      target: true
+    },
+  ];
+
+function resetForm(formEl) {
+  formEl?.resetFields();
+}
+
+function submitForm(formEl) {
+  formEl?.validate(async (valid) => {
+    if (valid) {
+      const pageUrl = window.location.href,
+        body = JSON.stringify({ ...form, pageUrl, pageTitle });
+      try {
+        ElMessage("Отправка запроса...");
+        const response = await fetch("https://bryusova.vues3.workers.dev", { method, headers, body });
+        if (response.ok) {
+          await response.json();
+          ElMessage.closeAll();
+          ElMessage.success(message);
+          formEl?.resetFields();
+        } else {
+          const { message } = await response.json();
+          ElMessage.closeAll();
+          ElMessage.error(`Ошибка: ${message}`);
+        }
+      } catch (e) {
+        ElMessage.closeAll();
+        ElMessage.error(`Ошибка: ${e.message}`);
+      }
+    } else ElMessage.error("Пожалуйста, заполните форму");
+  })
+}
 </script>
