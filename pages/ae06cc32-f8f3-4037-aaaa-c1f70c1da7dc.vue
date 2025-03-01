131d2decdf75403b38b4cc80989d6b032b080677
briusova <briusova@gmail.com>
2025-03-01 22:54:25 +0200

    услуги

--- pages/ae06cc32-f8f3-4037-aaaa-c1f70c1da7dc.vue
+++ pages/ae06cc32-f8f3-4037-aaaa-c1f70c1da7dc.vue
@@ -4,8 +4,9 @@
             <div class="max-w-xl space-y-3 not-prose mb-6">
                 <h3 class="text-indigo-600 font-semibold">{{ the.name }}</h3>
                 <p class="text-gray-800 text-3xl font-semibold sm:text-4xl">{{ the.title }}</p>
-                <p>{{ the.description }} </p>
-<p>                    Очные консультации в Калининграде. Онлайн по всему миру.</p>
+                <p>Перед консультацией всегда возникает много вопросов: «Смогу ли я открыться? Поймёт ли меня психолог? Будет ли результат?» — и самый важный из них: «Получится ли изменить то, что не устраивает в жизни?»</p>
+
+<p>Вы — автор своей жизни, я — наставник, который помогает освоить «грамматику» перемен. От букв (понимание себя) — к слогам (навыкам), от слов (решений) — к предложениям (вашей новой жизни). Ключ к успеху — готовность начать, доверие и терпение.</p>
             </div>
 
 
@@ -16,7 +17,7 @@
                         class="max-w-md mx-auto mt-4 shadow-lg border rounded-md duration-300 hover:shadow-sm">
                         <router-link :to="to">
                             <el-image :src="images[0].url" lazy :alt="images[0].alt" fit="cover"
-                                class="w-full h-auto rounded-t-md" />
+                                class="w-full h-auto rounded-t-md"></el-image>
                             <div class="pt-3 ml-4 mr-2 mb-3">
                                 <h3 class="text-xl text-gray-900">
                                     {{ title }}
